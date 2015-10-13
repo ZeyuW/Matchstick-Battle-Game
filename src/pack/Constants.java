99df74a423ed21dbcfc5cbc4bf89627574729fd4
ZeyuW <simplee@umich.edu>
2015-10-13 13:57:12 -0400

    update

---
+++ src/pack/Constants.java
@@ -0,0 +1,56 @@
+package pack;
+
+public class Constants
+{
+  public static final double GAME_TIME = 60.0;
+  public static final int PLAYER_HEALTH = 100;
+  public static final double NORMAL_ATTACK = 1.0;
+  public static final double SPECIAL_ATTACK = 25.0;
+  
+  //PLAYER CONSTANTS
+  public static final int JUMP_HEIGHT = 15;
+  public static final double MOVE_SPEED = 13.0;
+  public static final double DOWN_SPEED = 15.0;
+  
+  //SPECIAL ATTACK RANGE
+  public static final double SPECIAL_ATTACK_HEIGHT = 20.0;
+  public static final double SPECIAL_ATTACK_LENGTH = 100.0;
+  public static final double SPECIAL_TIME = 1.0;
+  
+  //MEGA
+  public static final double HIGHEST_MEGA = 100.0;
+  public static final double HIT_INCREASE_MEGA = 10.0;
+  
+  //TIME
+  public static final double SUPER_TIME = 3.0;
+  public static final double REBORN_TIME = 5.0;
+  
+  public static final double LOWER_BOUND = -100.0;
+  public static final int STARTX = 100;
+  public static final int STARTY = 100;
+  
+  //HIT DISTANCE
+  public static final int MINIMUM_HIT_D = 80;
+  //MAP CONSTANTS
+  public static final double FLOOR_HEIGHT_FLOWER = 200.0;
+  public static final double FLOOR_HEIGHT_GRASS = 400.0;
+  public static final double FLOOR_HEIGHT_SNOW = 530.0;
+  public static final double FLOORX_RANGE1 = 1;
+  public static final double FLOORX_RANGE2 = 2;
+  
+  public static final double HOLE_X_LEFT = 470.0;
+  public static final double HOLE_X_RIGHT = 630.0;
+  public static final double FLOWER_Y = 300.0;
+//  public static final double HOLE_X_RIGHT = 650.0;
+//  public static final double FLOORX_RANGE1 = 1;
+//  public static final double FLOORX_RANGE2 = 2;
+  
+  public static final int INCREASE_POINT = 5;
+  
+  public static final int BACK_STEP=4;
+  public static final int HIT_BY_NORMAL = 2;
+  public static final int HIT_BY_SPECIAL = 5;
+  
+  public static final int JUMP_STEP=7;
+  public static final int JUMP_HEIGHT_PERSTEP=20;
+}
